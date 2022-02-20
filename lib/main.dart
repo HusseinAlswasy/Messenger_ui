035fdf5bc0c93530262b39d95b9fdd5f6f92fe6b
HusseinAlswasy <01096694296hud@gmail.com>
2022-02-20 17:25:40 +0200

    Counter_Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:messenger_ui/counter_screen.dart';
 import 'package:messenger_ui/messenger_screen.dart';
 import 'package:messenger_ui/users_screen.dart';
 import 'package:messenger_ui/whatsapp.dart';
 import 'users_screen.dart';
+import 'counter_screen.dart';
 void main(){
-  return runApp(MyApp());
+  return runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
@@ -12,9 +14,9 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:UserScreen(),
+      home: CounterScreen(),
     );
   }
 }
