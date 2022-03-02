b36f143e7e42628ecad4505e93ddccc63aec412c
HusseinAlswasy <01096694296hud@gmail.com>
2022-03-02 12:17:18 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:messenger_ui/messenger_screen.dart';
-import 'package:messenger_ui/users_screen.dart';
-import 'package:messenger_ui/whatsapp.dart';
-import 'users_screen.dart';
+
+import 'moduls/counter_screen/counter_screen.dart';
+// import '../moduls/counter_screen/counter_screen.dart';
+// import '../moduls/messenger_screen/messenger_screen.dart';
+// import '../moduls/users_screen/users_screen.dart';
+// import '../moduls/whatsapp/whatsapp.dart';
+// import '../moduls/users_screen/users_screen.dart';
+
 void main(){
-  return runApp(MyApp());
+  return runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget{
@@ -12,9 +16,9 @@ class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:UserScreen(),
+      home: CounterScreen(),
     );
   }
 }
